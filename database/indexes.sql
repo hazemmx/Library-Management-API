9311b6a636da5c654466bb6ae277be5d5554809c
hazem <hazemgobran75@gmail.com>
2026-01-31 21:16:49 +0200

    updated indexes

--- database/indexes.sql
+++ database/indexes.sql
@@ -1,3 +1,11 @@
+-- Basic Search
+CREATE INDEX idx_book_isbn ON books(isbn);
 CREATE INDEX idx_book_title ON books(title);
 CREATE INDEX idx_book_author ON books(author);
-CREATE INDEX idx_book_isbn ON books(isbn);
+
+-- Relational Lookups 
+CREATE INDEX idx_borrowing_book_id ON borrowings(book_id);
+CREATE INDEX idx_borrowing_borrower_id ON borrowings(borrower_id);
+
+-- Reporting Lookups
+CREATE INDEX idx_borrowing_status ON borrowings(return_date) WHERE return_date IS NULL;
